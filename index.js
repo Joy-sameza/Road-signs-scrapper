f65971e770d902a9fbb1a00c9d489815094c8ca0
Joy-sameza <101399643+Joy-sameza@users.noreply.github.com>
2024-06-14 13:34:24 +0100

    upload artifact

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const x = require("./scrape");
 async function main() {
   const roadSigns = await x();
   if (roadSigns)
-    fs.writeFileSync("dist/road_signs.json", JSON.stringify(roadSigns, null, 2));
+    fs.writeFileSync("road_signs.json", JSON.stringify(roadSigns, null, 2));
 }
 
 console.time("Started scrapping");
